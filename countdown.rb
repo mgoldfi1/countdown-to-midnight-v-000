1e7ec372ea26984f56df64e2c196e01670931be8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-19 17:26:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,14 @@ def countdown(integer)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+  
